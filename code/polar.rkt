7932b446dcd2b435c46b5795c390c15e2524d009
kokatoo <kokatoo1948@gmail.com>
2020-10-16 17:31:23 +0800

    Fix for mobile phone

--- code/polar.rkt
+++ code/polar.rkt
@@ -36,16 +36,29 @@
        0
        (* 2 pi)))
 
-(plot (parametric
-       (lambda (theta)
-         (polar->cartesian theta (cos (* 2 theta))))
-       0
-       (* 2 pi)))
+(define (ex)
+  (define f (new frame% [label "Test graph"]
+                 [width 600]
+                 [height 800]))
+
+  (define c1 (new canvas% [parent f]
+                  [min-height 300]))
+
+  (define c2 (new canvas% [parent f]
+                  [min-height 300]))
 
 
+  (send f show #t)
 
-(plot (parametric
-       (lambda (r)
-         (polar->cartesian pi r)) -2 1))
-
-
+  (define panel (new horizontal-panel% [parent f]
+                     [alignment '(center center)]))
+  (new button% [parent panel]
+       [label "Start"]
+       [callback (lambda (button event)
+                   #f)])
+  (new button% [parent panel]
+       [label "Stop"]
+       [callback (lambda (button event)
+                   #f)])
+  #f)
+(ex)
